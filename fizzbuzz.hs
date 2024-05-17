1bd76804b3b3ba9a99db21a60958d67bc29341d0
Helge Holm <helge.holm@gmail.com>
2024-05-18 01:39:38 +0200

    Worse haskell

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,6 +1,5 @@
-import Data.Maybe
-fizz = [Nothing, Nothing, Just "fizz"] ++ fizz
-buzz = [Nothing, Nothing, Nothing, Nothing, Just "buzz"] ++ buzz
-calc x = if (length x) > 1 then (drop 1 x) else x
-fizzbuzz = map (concat . calc . catMaybes . (\(a, b, c) -> [a, b, c])) $ zip3 (map (Just . show) [1..]) fizz buzz
+fizz = []:[]:["fizz"]:fizz
+buzz = []:[]:[]:[]:["buzz"]:buzz
+calc c@(x:xs) = drop (signum . length $ xs) c
+fizzbuzz = map (concat . calc . (\(a, b, c) -> a ++ b ++ c)) $ zip3 (map (return . show) [1..]) fizz buzz
 main = mapM_ putStrLn $ take 100 fizzbuzz
